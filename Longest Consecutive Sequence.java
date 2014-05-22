b06b576db547328e9fe91e29bc7818db09c7a39b
zhujiaqi517 <zhujiaqi@ufl.edu>
2014-05-21 21:35:48 -0400

    new file
    lcs

---
+++ Longest Consecutive Sequence.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int longestConsecutive(int[] num) {
+    	HashSet<Integer> set = new HashSet<Integer>();
+    	int max = 1;
+     
+    	for (int e : num)
+    		set.add(e);
+     
+    	for (int e : num) {
+    		int left = e - 1;
+    		int right = e + 1;
+    		int count = 1;
+     
+    		while (set.contains(left)) {
+    			count++;
+    			set.remove(left);
+    			left--;
+    		}
+     
+    		while (set.contains(right)) {
+    			count++;
+    			set.remove(right);
+    			right++;
+    		}
+     
+    		max = Math.max(count, max);
+    	}
+     
+    	return max;
+    }
+}
