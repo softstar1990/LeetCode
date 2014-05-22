6186ae685e076089c10da9439ee33a9fea4b21c1
Jiaqi Zhu <zhujiaqi.apply@gmail.com>
2014-05-22 15:52:57 -0400

    Create Palindrome Number.java

---
+++ Palindrome Number.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+ 
+		int div = 1;
+		while (x / div >= 10) {
+			div *= 10;
+		}
+ 
+		while (x != 0) {
+			int left = x / div;
+			int right = x % 10;
+ 
+			if (left != right)
+				return false;
+ 
+			x = (x % div) / 10;
+			div /= 100;
+		}
+ 
+		return true;
+    }
+}
