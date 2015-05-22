d46ac37728026aebe9138d5d3eb61e90f491753d
Jiaqi Zhu <zhujiaqi.apply@gmail.com>
2015-05-22 10:17:59 -0400

    Update Unique_Binary_Search_Trees.java

--- 07_Binary_Tree/Binary_Search_Tree/Unique_Binary_Search_Trees.java
+++ 07_Binary_Tree/Binary_Search_Tree/Unique_Binary_Search_Trees.java
@@ -19,3 +19,19 @@ public class Solution {
        return uniq;
     }
 }
+
+//another solution without recursion, seems faster, use an array to store the result
+public class Solution {
+    public int numTrees(int n) {       
+        int[] result = new int[n+1];
+        result[0] = 1;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                result[i] += result[j-1] * result[i-j];
+            }
+        }
+        
+        return result[n];
+    }
+}
